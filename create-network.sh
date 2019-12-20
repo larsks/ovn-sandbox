d7b4152a7fe82352895c54052f745df15bb3760e
Lars Kellogg-Stedman <lars@oddbit.com>
2019-12-20 11:40:23 -0500

    configure dynamic addressing

--- create-network.sh
+++ create-network.sh
@@ -11,10 +11,14 @@ $NB --may-exist ls-add net0
 # create net0 port for gateway
 $NB --may-exist lsp-add net0 net0-gw
 $NB lsp-set-addresses net0-gw "c0:ff:ee:00:00:01 10.0.0.1"
+$NB set logical_switch net0 \
+	other_config:subnet="10.0.0.0/24" \
+	other_config:exclude_ips="10.0.0.2..10.0.0.10"
 
 ovs-vsctl --may-exist add-port br-int net0-gw -- \
 	set interface net0-gw \
-	    type=internal mac='"c0:ff:ee:00:00:01"' \
+	    type=internal \
+	    mac='"c0:ff:ee:00:00:01"' \
 	    external_ids:iface-id=net0-gw
 
 dhcp_options=$($NB create dhcp_options \
@@ -27,10 +31,9 @@ for portnum in {1..3}; do
 
 	addr=$(( 10 + portnum ))
 	macaddr="c0:ff:ee:00:00:$addr"
-	ipaddr="10.0.0.$addr"
 
 	$NB --may-exist lsp-add net0 $port
-	$NB lsp-set-addresses $port "$macaddr $ipaddr"
+	$NB lsp-set-addresses $port "$macaddr dynamic"
 	$NB lsp-set-dhcpv4-options $port $dhcp_options
 done
 
